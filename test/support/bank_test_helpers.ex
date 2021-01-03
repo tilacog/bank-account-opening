9c537d671a251869ba0407de4b18152d1882d9de
tilacog <tilacog@protonmail.com>
2021-01-03 18:59:35 -0300

    fix test helper function signature

--- test/support/bank_test_helpers.ex
+++ test/support/bank_test_helpers.ex
@@ -2,8 +2,11 @@ defmodule Bank.TestHelpers do
   alias Bank.Auth
 
   def api_user_fixture(attrs \\ %{}) do
-    attrs
-    |> Enum.into(%{cpf: "00000000191", password: "01234567890"})
-    |> Auth.register_user()
+    {:ok, api_user} =
+      attrs
+      |> Enum.into(%{cpf: "00000000191", password: "01234567890"})
+      |> Auth.register_user()
+
+    api_user
   end
 end
