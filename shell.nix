479bbb9aca208467d913fb749c3740168ec3ac06
tilacog <tilacog@protonmail.com>
2021-01-04 12:12:57 -0300

    generate CLOAK_SECRET environment variable

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ with pkgs;
 mkShell {
   buildInputs = [ elixir_1_11 git postgresql_13 ];
   LANG="C.utf8";
+  CLOAK_SECRET="bezq7tbGMqgn9QLkuGJmFNfXec7jOLTKHz1bI0bYTHw=";
   # Put the PostgreSQL databases in the project diretory.
   shellHook = ''
       export PGDATA="$PWD/.db_data"
