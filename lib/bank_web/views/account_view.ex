2bc40729515d62b696e8cdce7d541ab2b7342ba4
tilacog <tilacog@protonmail.com>
2021-01-13 10:21:36 -0300

    minor refactor

--- lib/bank_web/views/account_view.ex
+++ lib/bank_web/views/account_view.ex
@@ -2,23 +2,20 @@ defmodule BankWeb.AccountView do
   use BankWeb, :view
 
   def render("partial_account.json", %{partial_account: pacc}) do
-    result =
-      Map.merge(
-        %{
-          id: pacc.id,
-          name: pacc.name,
-          email: pacc.email,
-          birth_date: pacc.birth_date,
-          gender: pacc.gender,
-          city: pacc.city,
-          state: pacc.state,
-          country: pacc.country,
-          referral_code: pacc.referral_code
-        },
-        completion_status(pacc)
-      )
-
-    result
+    Map.merge(
+      %{
+        id: pacc.id,
+        name: pacc.name,
+        email: pacc.email,
+        birth_date: pacc.birth_date,
+        gender: pacc.gender,
+        city: pacc.city,
+        state: pacc.state,
+        country: pacc.country,
+        referral_code: pacc.referral_code
+      },
+      completion_status(pacc)
+    )
   end
 
   def completion_status(partial_account) do
