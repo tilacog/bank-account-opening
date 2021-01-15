1e7a7e8704ffa7e7b11bb3d83f8ff8f15e3ffd93
tilacog <tilacog@protonmail.com>
2021-01-15 13:02:31 -0300

    add disclaimer

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ with pkgs;
 mkShell {
   buildInputs = [ elixir_1_11 git postgresql_13 httpie jq ];
   LANG="C.utf8";
-  CLOAK_SECRET_KEY="a+gYgCLyXaSXJjc+O3raTanCOUrP/PsuL/dWrzSzzTk=";
+  CLOAK_SECRET_KEY="a+gYgCLyXaSXJjc+O3raTanCOUrP/PsuL/dWrzSzzTk=";  # don't use commited secrets in production, ok?
   # Put the PostgreSQL databases in the project diretory.
   shellHook = ''
       export PGDATA="$PWD/.pg_data"
